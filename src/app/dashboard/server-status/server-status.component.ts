b601d1ccb717a245aef646bdb8b9ec6f3db211bb
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2025-07-11 02:20:03 +0400

    added destroyRef and other feature

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, DestroyRef, inject, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.component.css',
   standalone: true
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus : 'online' | 'offline' | 'unknown' = 'offline';
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+   const interval = setInterval(()=>{
+      const rnd = Math.random();
+
+      if(rnd < 0.5){
+        this.currentStatus = 'online';
+      }else if (rnd < 0.9){
+        this.currentStatus = 'offline';
+      }else {
+        this.currentStatus = 'unknown';
+      }
+    },5000)
+    this.destroyRef.onDestroy(()=>{
+      clearInterval(interval)
+      console.log("component destroyed")
+    })
+  }
+
+  ngAfterViewInit() {
+    console.log('after view init');
+  }
+
+  // ngOnDestroy() {
+  //
+  // }
 }
